c37cc6bb845916af7e3fb4b36e2cc2dffe0cb6a0
David Cantoran <davidcantoran36@gmail.com>
2019-12-26 19:31:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,15 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :
+  
   def initialize(left, right, base)
     @left = left 
     @right = right
     @base = base
+  end 
+  
+  def kind 
+    if @left == @right && @left == @base 
+      equilateral
+    end 
   end 
 end
