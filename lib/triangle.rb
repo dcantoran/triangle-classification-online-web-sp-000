e188619f66f8532d83f21614c7a8bdafa3e9c436
David Cantoran <davidcantoran36@gmail.com>
2019-12-26 19:42:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   end 
   
   def kind 
-    if @left > 0 && @right > 0 && @base > 0 && @left + @right > @base || @left + @base > @right || @right + @base > @left 
+    if greater_than_zero? && @left + @right > @base || @left + @base > @right || @right + @base > @left 
       :equilateral
     elsif 
     end 
